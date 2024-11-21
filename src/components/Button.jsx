799a8a2266cf6777341500266ff547c63ec3b7cd
antoni0neto <antonio.silva6@yahoo.com.br>
2024-11-21 12:46:15 -0300

    Initial commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+function Button(props) {
+  return (
+    <button
+      className="bg-slate-400 text-white p-2 rounded-md"
+      {...props}
+    ></button>
+  );
+}
+
+export default Button;
---
+++ src/components/Title.jsx
@@ -0,0 +1,9 @@
+function Title({ children }) {
+  return (
+    <h1 className="text-slate-100 font-bold text-center text-3xl">
+      {children}
+    </h1>
+  );
+}
+
+export default Title;
